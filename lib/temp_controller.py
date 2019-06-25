62361390f33d1bbb690ed84e84236f9914ec993b
lukas <wicki96@gmail.com>
2019-06-25 22:04:39 +0200

    logging Sensor id and temp every 5 seconds

--- lib/temp_controller.py
+++ lib/temp_controller.py
@@ -1,18 +1,19 @@
+import sched
+
 from w1thermsensor import W1ThermSensor
 import time
 
 
 class TempController:
+    s = sched.scheduler(time.time, time.sleep)
 
     def start(self):
-        sensors = W1ThermSensor.get_available_sensors()
+        self.s.enter(5, 1, self.read_temperatures)
+        self.s.run()
 
-        print(sensors)
-
-        for sensor in sensors:
-            print("Sensor " + sensor.id + " found")
-
-    def readTemperatures(self):
+    def read_temperatures(self):
         sensors = W1ThermSensor.get_available_sensors()
         for sensor in sensors:
-            print("Sensor "+sensor.id+" found: "+sensor.get_temperature())
+            print("Sensor " + sensor.id + " found: " + sensor.get_temperature())
+        self.s.enter(5, 1, self.read_temperatures)
+        self.s.run()
